873dda2247dc2fb87df296ab091cd0aa5171db8b
Deepak Meena <deepak07itmeena@gmail.com>
2024-02-11 04:36:20 +0530

    done crud and search with mongoose in nodejs

--- index.js
+++ index.js
@@ -33,4 +33,18 @@ app.put("/update/:_id", async (req, resp) => {
     resp.send(data);
 })
 
+app.get("/search/:key", async (req, resp) => {
+    console.log(req.params.key);
+    let data = await Product.find(
+        {
+            "$or": [
+                { name: { $regex: req.params.key } },
+                { brand: { $regex: req.params.key } },
+                { category: { $regex: req.params.key } }
+            ]
+        }
+    );
+    resp.send(data);
+})
+
 app.listen(5000);
