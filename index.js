fae7e8b73845851a0780b83ceb0a9cc0dab30a29
Deepak Meena <deepak07itmeena@gmail.com>
2024-02-10 17:28:33 +0530

    2nd commit

--- index.js
+++ index.js
@@ -1,18 +1,50 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
-const main = async () => {
-    await mongoose.connect("mongodb://localhost:27017/e-comm");
-    const ProductSchema = new mongoose.Schema({
-        name: String,
-        price: Number
-    })
-    const ProductModal = mongoose.model('products', ProductSchema);
-    let data = new ProductModal({ name: "m 10", price: 1000 });
-    let result = await data.save();
+mongoose.connect('mongodb://localhost:27017/e-comm');
+const productSchema = new mongoose.Schema({
+    name: String,
+    price: Number,
+    brand: String,
+    category: String
+
+});
+
+const saveInDB = async () => {
+    const Product = mongoose.model('products', productSchema);
+    let data = new Product({
+        name: "max 100",
+        price: 200,
+        brand: 'maxx',
+        category: 'Mobile'
+    });
+    const result = await data.save();
     console.log(result);
 }
 
-main()
+const updateInDB =async  () => {
+    const Product = mongoose.model('products', productSchema);
+    let data =await  Product.updateOne(
+        { name: "max 6" },
+        {
+            $set: { price: 550,name:'max pro 6' }
+        }
+    )
+    console.log(data)
+}
+
+const deleteInDB = async ()=>{
+    const Product = mongoose.model('products', productSchema);
+    let data = await Product.deleteOne({name:'max 100'})
+    console.log(data);
+}
+const findInDB = async ()=>{
+    const Product = mongoose.model('products', productSchema);
+    let data = await Product.find({name:'max pro 611'})
+    console.log(data);
+}
+
+findInDB();
+
 
 
 
