ed3d63ef11d75520a8ff753cbc74c15cc8c5af5a
Deepak Meena <deepak07itmeena@gmail.com>
2024-02-10 17:34:44 +0530

    done crud with mongoose in nodejs

--- data.js
+++
@@ -1,6 +0,0 @@
-const data = [
-    {name: 'Anil Sidhu', email: 'anil@test.com'},
-    {name: 'Anil Sidhu', email: 'anil@test.com'},
-    {name: 'Anil Sidhu', email: 'anil@test.com'},
-]
-module.exports=data
